2c12d3dbe55cb66183c8869a36a37a841ce3b84f
Tobias Boyer <tobias.boyer@outlook.com>
2025-01-28 20:03:32 +0200

    Update site URL and base path for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://elrathor.github.io/photon-lab/',
+  url: 'https://elrathor.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/photon-lab/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
